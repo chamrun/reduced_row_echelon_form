fe62ee1d3767d6630aab678f03f0c5ed56962451
ChamRun <chrm@aut.ac.ir>
2022-12-20 20:09:22 +0330

    Add inline comments to `reduced_row_echelon_form`

--- src/reduced_row_echelon_form.py
+++ src/reduced_row_echelon_form.py
@@ -8,6 +8,7 @@ def input_matrix() -> np.ndarray:
     Returns:
         np.ndarray: The matrix input by the user.
     """
+
     rows = int(input("Enter the number of rows: "))
     cols = int(input("Enter the number of columns: "))
     matrix = np.zeros((rows, cols))
@@ -28,13 +29,16 @@ def find_reduced_row_echelon_form(matrix: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: The reduced row echelon form of the matrix.
     """
+
     matrix = matrix.astype(float)
     rows, cols = matrix.shape
-    lead = 0
+    lead = 0  # lead is the index of the first non-zero element in a row
+
     for r in range(rows):
         if lead >= cols:
             return matrix
         i = r
+
         while matrix[i, lead] == 0:
             i += 1
             if i == rows:
@@ -42,12 +46,16 @@ def find_reduced_row_echelon_form(matrix: np.ndarray) -> np.ndarray:
                 lead += 1
                 if cols == lead:
                     return matrix
-        matrix[[i, r]] = matrix[[r, i]]
-        lv = matrix[r, lead]
-        matrix[r] = matrix[r] / lv
+
+        matrix[[i, r]] = matrix[[r, i]]  # Swap rows i and r to put the pivot on the diagonal.
+
+        pivot_value = matrix[r, lead]
+        matrix[r] = matrix[r] / pivot_value  # Divide row r by the pivot value.
+
         for i in range(rows):
             if i != r:
-                lv = matrix[i, lead]
-                matrix[i] = matrix[i] - lv * matrix[r]
+                row_i_value = matrix[i, lead]
+                matrix[i] = matrix[i] - row_i_value * matrix[r]  # Subtract row r from row i.
         lead += 1
+
     return matrix
