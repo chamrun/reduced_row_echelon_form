990de4182aaa7e2e77dcdcc49db4efca1057ac5e
ChamRun <chrm@aut.ac.ir>
2022-12-20 19:21:23 +0330

    add function `find_reduced_row_echelon_form`

--- src/calculator.py
+++ src/calculator.py
@@ -12,3 +12,28 @@ def input_matrix() -> np.ndarray:
 
     return matrix
 
+
+def find_reduced_row_echelon_form(matrix: np.ndarray) -> np.ndarray:
+    matrix = matrix.astype(float)
+    rows, cols = matrix.shape
+    lead = 0
+    for r in range(rows):
+        if lead >= cols:
+            return matrix
+        i = r
+        while matrix[i, lead] == 0:
+            i += 1
+            if i == rows:
+                i = r
+                lead += 1
+                if cols == lead:
+                    return matrix
+        matrix[[i, r]] = matrix[[r, i]]
+        lv = matrix[r, lead]
+        matrix[r] = matrix[r] / lv
+        for i in range(rows):
+            if i != r:
+                lv = matrix[i, lead]
+                matrix[i] = matrix[i] - lv * matrix[r]
+        lead += 1
+    return matrix
